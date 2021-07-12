b8d840fab50670f86a3efc6c6ef0b5f1432af864
Krishna-J005 <krishna.jaiswal@timesinternet.in>
2021-07-12 13:49:07 +0530

    update

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require('express'); //import express
-const server= express();// initialiase
+const express= require ('express'); //import express framework
+const server =express(); //initialize
 
-server.get('/', function(request,response){
-
-    console.log(request,response);
+server.get('/',function(request,response){
+    console.log(request.method);
+    console.log(request.query.query1);
+    console.log(request.query.query2);
+    console.log(request.headers);
+    console.log(request.url);
+    console.log('value of accept header='+request.headers.accept);
+    console.log('value of accept header='+request.headers['cache-control']);
+    console.log(request.response);
     response.send('Hello World');
-
-});
-server.listen(4000,function(){
-    console.log('server started .........');
+})
+server.listen(3000,function(){
+    console.log('server started');
 })
